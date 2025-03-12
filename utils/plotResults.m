d8c6b94ab7e52b43959fd591aefdf2a61e62f41e
David <david.moran@alu.uclm.es>
2025-03-12 18:36:33 +0100

    Added plots

--- utils/plotResults.m
+++ utils/plotResults.m
@@ -1,24 +1,27 @@
 %clear
 close all
 
-T_window_s = [200,50000];
+
 XSelect = 240;
 
-measuredMag = readtable("../build/Linux/tests/magLog.txt");
-accLog = readtable("../build/Linux/tests/accLog.txt");
+%measuredMag = readtable("../build/Linux/tests/magLog.txt");
+%accLog = readtable("../build/Linux/tests/accLog.txt");
 measuredQ = readtable("../build/Linux/tests/quatLog.txt");
-expectedQ = readtable("../build/Linux/tests/quatExpectedLog.txt");
+%expectedQ = readtable("../build/Linux/tests/quatExpectedLog.txt");
 estimatedQ = readtable("../build/Linux/tests/qEstLog.txt");
 measuredv = readtable("../build/Linux/tests/vLog.txt");
 PLog = readtable("../build/Linux/tests/PLog.txt");
 Pest = readtable("../build/Linux/tests/PestLog.txt");
 SLog = readtable("../build/Linux/tests/SLog.txt");
+HLog = readtable("../build/Linux/tests/HLog.txt");
 invSLog = readtable("../build/Linux/tests/invSLog.txt");
 FLog = readtable("../build/Linux/tests/FLog.txt");
 WLog = readtable("../build/Linux/tests/WLog.txt");
 QLog = readtable("../build/Linux/tests/QLog.txt");
 RLog = readtable("../build/Linux/tests/RLog.txt");
 KLog = readtable("../build/Linux/tests/KLog.txt");
+
+T_window_s = [0,3000];
 
 figure(1)
 subplot(1,2,1)
@@ -30,32 +33,41 @@ xline(XSelect);
 title("Predicted")
 xlim(T_window_s)
 
-subplot(1,2,2)
-plot(expectedQ.Variables)
-hold on
-xline(XSelect);
-title("Expected")
-xlim(T_window_s);
+% subplot(1,2,2)
+% plot(expectedQ.Variables)
+% hold on
+% xline(XSelect);
+% title("Expected")
+% xlim(T_window_s);
 
 rows = 4;
 col = 3;
 
 figure(2)
 subplot(rows,col,1)
+%%
 plot(measuredv.Variables)
 hold on
-xline(XSelect);
+%xline(XSelect);
 xlim(T_window_s)
 title("measurement-estimation divergence")
+legend
+%%
+% subplot(rows,col,2)
+% plot(measuredMag.Variables)
+% hold on
+% plot(accLog.Variables./10, '--')
+% xline(XSelect);
+% xlim(T_window_s)
+% title("measured mag-acc")
+% legend
 
 subplot(rows,col,2)
-plot(measuredMag.Variables)
-hold on
-plot(accLog.Variables./10, '--')
-xline(XSelect);
+plot(HLog.Variables)
+
 xlim(T_window_s)
-title("measured mag-acc")
-legend
+title("H")
+
 
 subplot(rows,col,3)
 plot(PLog.Variables)
@@ -88,30 +100,28 @@ title("P_est")
 subplot(rows,col,7)
 plot(estimatedQ.Variables)
 hold on
-xline(XSelect);
-xlim(T_window_s)
 title("q_est")
+legend
+xlim(T_window_s)
 
 subplot(rows,col,8)
 plot(QLog.Variables)
 hold on
-xline(XSelect);
-xlim(T_window_s)
 title("QLOg")
+xlim(T_window_s)
 
+ 
 subplot(rows,col,9)
 plot(RLog.Variables)
 hold on
-xline(XSelect);
-xlim(T_window_s)
 title("RLog")
+xlim(T_window_s)
 
 subplot(rows,col,10)
 plot(WLog.Variables)
 hold on
-xline(XSelect);
-xlim(T_window_s)
 title("WLog")
+xlim(T_window_s)
 
 subplot(rows,col,11)
 plot(invSLog.Variables)
